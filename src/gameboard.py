2a369ca84cd5ffc5abd4098b347b86c322ebf0dd
Susanna Huhtanen <ihmis.suski@gmail.com>
2010-07-23 14:56:17 +0300

    button signals

--- src/gameboard.py
+++ src/gameboard.py
@@ -213,7 +213,14 @@ class UI(hildon.StackableWindow):
             )
 
     def create_adventure(self, button):
-        print "Adventure clicked"
+        print " Adventure creation clicked"
+        def create_entry():
+            entry = hildon.Entry(gtk.HILDON_SIZE_AUTO)
+            entry.set_placeholder("51 28 38")
+            entr = hildon.Entry(gtk.HILDON_SIZE_AUTO)
+            entr.set_placeholder("25 28 38")
+            return entry
+
 
     def settings(self, button):
         print "Settings clicked"
