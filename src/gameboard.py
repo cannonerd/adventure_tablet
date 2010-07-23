86ea244e98792c1033b3477c16ca5e21a7eba1b2
Susanna Huhtanen <ihmis.suski@gmail.com>
2010-07-23 18:54:40 +0300

    button signals

--- src/gameboard.py
+++ src/gameboard.py
@@ -226,6 +226,8 @@ class UI(hildon.StackableWindow):
         wind.add(vbox)
         entry = hildon.Entry(gtk.HILDON_SIZE_AUTO)
         entry.set_placeholder("We're Going to...")
+        entry.add(vbox)
+        
         # This call show the window and also add the window to the stack
         wind.show_all()
 
