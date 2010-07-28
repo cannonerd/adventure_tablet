51296ab733177fbe43f561797cc0da479b32c1da
Susanna Huhtanen <ihmis.suski@gmail.com>
2010-07-29 00:28:38 +0300

    Send logs to Qaiku too

--- src/adventure.py
+++ src/adventure.py
@@ -4,6 +4,7 @@ import _midgard as midgard
 class adventure():
     mission = None
     destination = None
+    qaikuid = None
     name = ""
     adventurers = []
 
@@ -11,6 +12,8 @@ class adventure():
         self.destination = destination
         self.name = name
         self.mission = mission
+        if self.mission is not None:
+            self.qaikuid = self.mission.get_parameter('adventuretablet', 'qaikuid')
 
     def add_adventurer(self, adventurer):
         self.adventurers.append(adventurer)
@@ -19,6 +22,11 @@ class adventure():
     def remove_adventurer(self, adventurer):
         self.adventurers.remove(adventurer)
         adventurer.disconnect(adventurer.mission_listener)
+
+    def set_qaikuid(self, qaikuid):
+        self.qaikuid = qaikuid
+        if self.mission is not None:
+            self.mission.set_parameter('adventuretablet', 'qaikuid', qaikuid)
 
     def update_log(self, adventurer, location, data = None, text = None):
         log = midgard.mgdschema.ttoa_log()
@@ -31,6 +39,31 @@ class adventure():
             log.comment = text
         log.participating = True
         log.create()
+        if adventurer.apikey is not None:
+            self.log_to_qaiku(self, log, adventurer.apikey)
+
+    def log_to_qaiku(self, adventure, log, apikey):
+        if adventure.qaikuid is None:
+            return
+
+        if log.text == '':
+            log.text = 'Adventuring'
+
+        opener = urllib2.build_opener()
+        opener.addheaders = [('User-agent', 'adventure_tablet/0.1')]
+        try:
+            data = urllib.urlencode({'status': unicode(log.text).encode('utf-8'), 'source': 'adventuretablet', 'channel': 'adventure', 'latitude': log.latitude, 'longitude': log.longitude, 'in_reply_to_status_id': adventure.qaikuid})
+            params = urllib.urlencode({'apikey': apikey})
+            url = 'http://www.qaiku.com/api/statuses/update.json?%s' % params
+            req = opener.open(url, data)
+            response = req.read()
+        except urllib2.HTTPError, e:
+            print "Updating failed, HTTP %s" % (e.code)
+        except urllib2.URLError, e:
+            print "Connection failed, error %s" % (e.message)
+
+        qaiku = simplejson.loads(response)
+        log.set_parameter('adventuretablet', 'qaikuid', qaiku.id)
 
     def adventure_to_qaiku(self, adventure, apikey):
         opener = urllib2.build_opener()
@@ -45,4 +78,6 @@ class adventure():
             print "Updating failed, HTTP %s" % (e.code)
         except urllib2.URLError, e:
             print "Connection failed, error %s" % (e.message)
-        print response
+
+        qaiku = simplejson.loads(response)
+        adventure.set_qaikuid(qaiku.id)
