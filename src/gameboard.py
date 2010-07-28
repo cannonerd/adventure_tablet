9a780219f420d298888610578e2975a2b4f27551
Susanna Huhtanen <ihmis.suski@gmail.com>
2010-07-28 20:54:29 +0300

    change the way we update player colour

--- src/gameboard.py
+++ src/gameboard.py
@@ -291,6 +291,8 @@ class UI(hildon.StackableWindow):
             colour_image = gtk.Image()
             colour_image.set_from_pixbuf(colour_pixbuf)
             colour_button = gtk.RadioButton(radioGroup)
+            if self.player.colour is colour:
+                colour_button.set_active(True)
             # Make the selectors look like buttons (Maemo 5 style compatibility, see bug 4578)
             colour_button.set_mode(False)
             if radioGroup is None:
