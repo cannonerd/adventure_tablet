91e6264dc843adb3e611c89f6fed168a694d4f2b
Susanna Huhtanen <ihmis.suski@gmail.com>
2010-07-30 14:12:29 +0300

    fixing typo

--- src/adventure.py
+++ src/adventure.py
@@ -161,7 +161,7 @@ class adventure(gobject.GObject):
                 log.comment = "Has arrived to destination %s." % (self.destination.describe())
             else:
                 if adventurer.location.distance_to(self.destination) <= 2:
-                    log.comment = 'Adventuring to %s, distance to destination %s km' %(self.destination.describe(), round(adventurer.location.distance_to(self.destination)), 2)
+                    log.comment = 'Adventuring to %s, distance to destination %s km' %(self.destination.describe(), round(adventurer.location.distance_to(self.destination), 2))
                 else:
                     log.comment = 'Adventuring to %s, distance to destination %s km' %(self.destination.describe(), int(adventurer.location.distance_to(self.destination)))
 
