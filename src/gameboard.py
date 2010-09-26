49e454ea78c8d989a3956e26b193bd013719a1d6
Susanna Huhtanen <ihmis.suski@gmail.com>
2010-09-26 14:10:12 +0300

    meddling with hildon touch selector

--- src/gameboard.py
+++ src/gameboard.py
@@ -195,8 +195,8 @@ class UI(hildon.StackableWindow):
             # Start polling Qaiku
             self.current_adventure.polling_timeout = gobject.timeout_add(30000, self.current_adventure.logs_from_qaiku, self.player)
 
-    def changed_adventure(self, TouchSelector, user_data):#combobox):
-        model = TouchSelector.get_model(0)
+    def changed_adventure(self, TouchSelector, user_data):
+        model = TouchSelector.get_model(adventure.combo_index)
         index = TouchSelector.get_active(0)
         if index is 0:
             # "Select adventure" selected
