589e719535c2c508920ec354bae9f3bc6960f2f8
Susanna Huhtanen <ihmis.suski@gmail.com>
2010-09-26 17:56:01 +0300

    from the basics

--- src/gameboard.py
+++ src/gameboard.py
@@ -100,7 +100,7 @@ class UI(hildon.StackableWindow):
         self.osm.set_keyboard_shortcut(osmgpsmap.KEY_RIGHT, gtk.gdk.keyval_from_name("Right"))
 
 #        self.adventure_selector = gtk.combo_box_new_text()
-        self.adventure_selector = hildon.TouchSelector()
+        self.adventure_selector = hildon.hildon_touch_selector_new_text()
         self.adventure_selector.append_text('Select adventure:')
         self.adventure_selector_position = 0
         for adventure in self.blyton.adventures:
